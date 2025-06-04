dd65ea3d3eada4575da6eca9a843215714236547
ADHISH45 <adhishachu4@gmail.com>
2025-06-04 14:51:52 +0530

    bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Designers from './pages/Designers'
 import Login from './pages/login'
 import About from './pages/about'
